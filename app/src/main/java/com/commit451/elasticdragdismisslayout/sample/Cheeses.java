c45cb1ce507977bd7e54c1e9f7985dde3806bca2
Pedro Laviola <pedrolaviola@ioasys.com.br>
2020-08-27 16:26:34 -0300

    teste

--- app/src/main/java/com/commit451/elasticdragdismisslayout/sample/Cheeses.java
+++ app/src/main/java/com/commit451/elasticdragdismisslayout/sample/Cheeses.java
@@ -22,6 +22,8 @@ public class Cheeses {
         }
     }
 
+    //teste develop
+
     public static final String[] sCheeseStrings = {
             "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
             "Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale",
